0628865690e7032f68a9dc4ada66e309f1570f8c
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-06-17 03:42:16 +0200

    WIP

---
+++ 3DUI/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float m_speed = 2.0f;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+            transform.position = Vector3.Lerp(transform.position, transform.position + Input.GetAxis("Vertical") * transform.forward, Time.deltaTime * m_speed);
+
+        Debug.DrawLine(transform.position, transform.position + Input.GetAxis("Vertical") * transform.forward, Color.red);
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+}
