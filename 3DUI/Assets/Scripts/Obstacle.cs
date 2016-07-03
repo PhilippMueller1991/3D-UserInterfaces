2e0a7e3cd0937e9687b83d960545cf2da1212fb1
Philipp Müller <philipp_mueller1991@yahoo.de>
2016-07-03 21:20:30 +0200

    Added obstacle script

---
+++ 3DUI/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour
+{
+    public float m_resetDistance = 0.001f;
+    public int m_damage = 10;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "Player")
+            return;
+
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        player.LoseLife((uint)m_damage);
+    }
+}
